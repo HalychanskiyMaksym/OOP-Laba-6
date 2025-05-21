30486184fba7b7cf8a7126f9c7e2e44effc0cafb
HalychanskiyMaksym <riro1727@gmail.com>
2025-05-21 22:26:47 +0300

    Update Task1.cpp

--- Task1.cpp
+++ Task1.cpp
@@ -4,259 +4,180 @@
 #include <ctime>
 using namespace std;
 
-// ===== Базовий клас без virtual inheritance =====
-class Base {
+// ======= Ієрархія без віртуального успадкування =======
+
+class A {
 protected:
-    int baseData;
+    int value;
 public:
-    Base() {
-        baseData = rand() % 100;
-        cout << "[Base] Created with baseData = " << baseData << endl;
+    A() : value(0) { cout << "A створено\n"; }
+    virtual ~A() { cout << "A знищено\n"; }
+
+    virtual void info() const {
+        cout << "Це клас A, значення = " << value << endl;
     }
-    virtual ~Base() {
-        cout << "[Base] Destroyed" << endl;
+
+    void inputFromKeyboard() {
+        cout << "[Клавіатура] Введіть значення для A: ";
+        cin >> value;
     }
-    virtual void show() const {
-        cout << "[Base] baseData = " << baseData << endl;
+
+    void inputFromFile(const string& filename) {
+        ifstream fin(filename);
+        if (fin >> value)
+            cout << "[Файл] Значення для A: " << value << endl;
+        else
+            cout << "[Файл] Помилка читання!" << endl;
+        fin.close();
     }
-    void setFromKeyboard() {
-        cout << "Enter baseData: ";
-        cin >> baseData;
-    }
-    void setFromFile(ifstream &fin) {
-        fin >> baseData;
+
+    void inputRandom() {
+        value = rand() % 100;
+        cout << "[Random] Згенеровано значення для A: " << value << endl;
     }
 };
 
-class Left1 : public Base {
+class B : public A {
+public:
+    B() { cout << "B створено\n"; }
+    ~B() { cout << "B знищено\n"; }
+
+    void info() const override {
+        cout << "Це клас B, значення = " << value << endl;
+    }
+};
+
+class C : public A {
+public:
+    C() { cout << "C створено\n"; }
+    ~C() { cout << "C знищено\n"; }
+
+    void info() const override {
+        cout << "Це клас C, значення = " << value << endl;
+    }
+};
+
+class D : public B {
+public:
+    D() { cout << "D створено\n"; }
+    ~D() { cout << "D знищено\n"; }
+};
+
+class E : public C {
+public:
+    E() { cout << "E створено\n"; }
+    ~E() { cout << "E знищено\n"; }
+};
+
+class F : public D, public E {
+public:
+    F() { cout << "F створено (без virtual)\n"; }
+    ~F() { cout << "F знищено (без virtual)\n"; }
+};
+
+// ======= Ієрархія з віртуальним успадкуванням =======
+
+class VA {
 protected:
-    int left1Data;
+    int value;
 public:
-    Left1() {
-        left1Data = rand() % 100;
-        cout << "[Left1] Created with left1Data = " << left1Data << endl;
+    VA() : value(0) { cout << "VA створено\n"; }
+    virtual ~VA() { cout << "VA знищено\n"; }
+
+    virtual void info() const {
+        cout << "Це клас VA, значення = " << value << endl;
     }
-    ~Left1() {
-        cout << "[Left1] Destroyed" << endl;
+
+    void inputFromKeyboard() {
+        cout << "[Клавіатура] Введіть значення для VA: ";
+        cin >> value;
     }
-    void show() const override {
-        Base::show();
-        cout << "[Left1] left1Data = " << left1Data << endl;
+
+    void inputFromFile(const string& filename) {
+        ifstream fin(filename);
+        if (fin >> value)
+            cout << "[Файл] Значення для VA: " << value << endl;
+        else
+            cout << "[Файл] Помилка читання!" << endl;
+        fin.close();
     }
-    void setFromKeyboard() {
-        Base::setFromKeyboard();
-        cout << "Enter left1Data: ";
-        cin >> left1Data;
-    }
-    void setFromFile(ifstream &fin) {
-        Base::setFromFile(fin);
-        fin >> left1Data;
+
+    void inputRandom() {
+        value = rand() % 100;
+        cout << "[Random] Згенеровано значення для VA: " << value << endl;
     }
 };
 
-class Right1 : public Base {
-protected:
-    int right1Data;
+class VB : virtual public VA {
 public:
-    Right1() {
-        right1Data = rand() % 100;
-        cout << "[Right1] Created with right1Data = " << right1Data << endl;
-    }
-    ~Right1() {
-        cout << "[Right1] Destroyed" << endl;
-    }
-    void show() const override {
-        Base::show();
-        cout << "[Right1] right1Data = " << right1Data << endl;
-    }
-    void setFromKeyboard() {
-        Base::setFromKeyboard();
-        cout << "Enter right1Data: ";
-        cin >> right1Data;
-    }
-    void setFromFile(ifstream &fin) {
-        Base::setFromFile(fin);
-        fin >> right1Data;
+    VB() { cout << "VB створено\n"; }
+    ~VB() { cout << "VB знищено\n"; }
+
+    void info() const override {
+        cout << "Це клас VB, значення = " << value << endl;
     }
 };
 
-class TopNoVirtual : public Left1, public Right1 {
-    int topData;
+class VC : virtual public VA {
 public:
-    TopNoVirtual() {
-        topData = rand() % 100;
-        cout << "[TopNoVirtual] Created with topData = " << topData << endl;
-    }
-    ~TopNoVirtual() {
-        cout << "[TopNoVirtual] Destroyed" << endl;
-    }
-    void show() const {
-        cout << "\n=== Дані з лівої гілки ===\n";
-        Left1::show();
-        cout << "\n=== Дані з правої гілки ===\n";
-        Right1::show();
-        cout << "[TopNoVirtual] topData = " << topData << endl;
-    }
-    void setFromKeyboard() {
-        cout << "\n--- Введення Left1 ---\n";
-        Left1::setFromKeyboard();
-        cout << "\n--- Введення Right1 ---\n";
-        Right1::setFromKeyboard();
-        cout << "Enter topData: ";
-        cin >> topData;
-    }
-    void setFromFile(ifstream &fin) {
-        Left1::setFromFile(fin);
-        Right1::setFromFile(fin);
-        fin >> topData;
+    VC() { cout << "VC створено\n"; }
+    ~VC() { cout << "VC знищено\n"; }
+
+    void info() const override {
+        cout << "Це клас VC, значення = " << value << endl;
     }
 };
 
-// ===== Базовий клас з virtual inheritance =====
-class BaseV {
-protected:
-    int baseDataV;
+class VD : public VB {
 public:
-    BaseV() {
-        baseDataV = rand() % 100;
-        cout << "[BaseV] Created with baseDataV = " << baseDataV << endl;
-    }
-    virtual ~BaseV() {
-        cout << "[BaseV] Destroyed" << endl;
-    }
-    virtual void show() const {
-        cout << "[BaseV] baseDataV = " << baseDataV << endl;
-    }
-    void setFromKeyboard() {
-        cout << "Enter baseDataV: ";
-        cin >> baseDataV;
-    }
-    void setFromFile(ifstream &fin) {
-        fin >> baseDataV;
+    VD() { cout << "VD створено\n"; }
+    ~VD() { cout << "VD знищено\n"; }
+};
+
+class VE : public VC {
+public:
+    VE() { cout << "VE створено\n"; }
+    ~VE() { cout << "VE знищено\n"; }
+};
+
+class VF : public VD, public VE {
+public:
+    VF() { cout << "VF створено (з virtual)\n"; }
+    ~VF() { cout << "VF знищено (з virtual)\n"; }
+
+    void info() const override {
+        cout << "Це клас VF, значення = " << value << endl;
     }
 };
 
-class LeftV1 : virtual public BaseV {
-protected:
-    int left1DataV;
-public:
-    LeftV1() {
-        left1DataV = rand() % 100;
-        cout << "[LeftV1] Created with left1DataV = " << left1DataV << endl;
-    }
-    ~LeftV1() {
-        cout << "[LeftV1] Destroyed" << endl;
-    }
-    void show() const override {
-        BaseV::show();
-        cout << "[LeftV1] left1DataV = " << left1DataV << endl;
-    }
-    void setFromKeyboard() {
-        BaseV::setFromKeyboard();
-        cout << "Enter left1DataV: ";
-        cin >> left1DataV;
-    }
-    void setFromFile(ifstream &fin) {
-        BaseV::setFromFile(fin);
-        fin >> left1DataV;
-    }
-};
+// ======= Виведення розмірів =======
 
-class RightV1 : virtual public BaseV {
-protected:
-    int right1DataV;
-public:
-    RightV1() {
-        right1DataV = rand() % 100;
-        cout << "[RightV1] Created with right1DataV = " << right1DataV << endl;
-    }
-    ~RightV1() {
-        cout << "[RightV1] Destroyed" << endl;
-    }
-    void show() const override {
-        BaseV::show();
-        cout << "[RightV1] right1DataV = " << right1DataV << endl;
-    }
-    void setFromKeyboard() {
-        BaseV::setFromKeyboard();
-        cout << "Enter right1DataV: ";
-        cin >> right1DataV;
-    }
-    void setFromFile(ifstream &fin) {
-        BaseV::setFromFile(fin);
-        fin >> right1DataV;
-    }
-};
+void printSizes() {
+    cout << "\n[Розміри об'єктів]\n";
+    cout << "F (без virtual): " << sizeof(F) << " байт\n";
+    cout << "VF (з virtual): " << sizeof(VF) << " байт\n";
+}
 
-class TopVirtual : public LeftV1, public RightV1 {
-    int topDataV;
-public:
-    TopVirtual() {
-        topDataV = rand() % 100;
-        cout << "[TopVirtual] Created with topDataV = " << topDataV << endl;
-    }
-    ~TopVirtual() {
-        cout << "[TopVirtual] Destroyed" << endl;
-    }
-    void show() const override {
-        cout << "\n=== Virtual Inheritance Tree ===\n";
-        LeftV1::show();
-        RightV1::show();
-        cout << "[TopVirtual] topDataV = " << topDataV << endl;
-    }
-    void setFromKeyboard() {
-        cout << "\n--- Введення LeftV1 ---\n";
-        LeftV1::setFromKeyboard();
-        cout << "\n--- Введення RightV1 ---\n";
-        RightV1::setFromKeyboard();
-        cout << "Enter topDataV: ";
-        cin >> topDataV;
-    }
-    void setFromFile(ifstream &fin) {
-        LeftV1::setFromFile(fin);
-        RightV1::setFromFile(fin);
-        fin >> topDataV;
-    }
-};
+// ======= Головна функція =======
 
 int main() {
-    srand(time(nullptr));
+    srand(time(0)); // ініціалізація генератора випадкових чисел
 
-    // Розміри класів
-    cout << "\n=== Розміри класів (без virtual) ===\n";
-    cout << "sizeof(Base) = " << sizeof(Base) << " байт" << endl;
-    cout << "sizeof(Left1) = " << sizeof(Left1) << " байт" << endl;
-    cout << "sizeof(Right1) = " << sizeof(Right1) << " байт" << endl;
-    cout << "sizeof(TopNoVirtual) = " << sizeof(TopNoVirtual) << " байт" << endl;
+    cout << "=== Ієрархія без віртуального успадкування ===\n";
+    F f;
+    ((B&)f).inputFromKeyboard();
+    ((B&)f).inputFromFile("input.txt");
+    ((B&)f).inputRandom();
+    ((B&)f).info();
 
-    cout << "\n=== Розміри класів (з virtual) ===\n";
-    cout << "sizeof(BaseV) = " << sizeof(BaseV) << " байт" << endl;
-    cout << "sizeof(LeftV1) = " << sizeof(LeftV1) << " байт" << endl;
-    cout << "sizeof(RightV1) = " << sizeof(RightV1) << " байт" << endl;
-    cout << "sizeof(TopVirtual) = " << sizeof(TopVirtual) << " байт" << endl;
+    cout << "\n=== Ієрархія з віртуальним успадкуванням ===\n";
+    VF vf;
+    ((VB&)vf).inputFromKeyboard();
+    ((VB&)vf).inputFromFile("input.txt");
+    ((VB&)vf).inputRandom();
+    vf.info();
 
-    // Введення з клавіатури
-    cout << "\n--- Введення TopNoVirtual з клавіатури ---\n";
-    TopNoVirtual obj1;
-    obj1.setFromKeyboard();
-    obj1.show();
-
-    cout << "\n--- Введення TopVirtual з клавіатури ---\n";
-    TopVirtual obj2;
-    obj2.setFromKeyboard();
-    obj2.show();
-
-    // Зчитування з файлу
-    ifstream fin("C:/Users/Maks/Desktop/Laba2/task1.txt");
-    if (fin) {
-        cout << "\n--- Зчитування TopNoVirtual з файлу ---\n";
-        TopNoVirtual obj3;
-        obj3.setFromFile(fin);
-        obj3.show();
-        fin.close();
-    } else {
-        cout << "\nНе вдалося відкрити файл!\n";
-    }
+    printSizes();
 
     return 0;
 }
